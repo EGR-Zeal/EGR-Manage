7338b8cda61555bddec2552aa5963dfd968bf6cb
EGR-Zeal <zeal@egrobotics.com>
2015-01-19 15:45:13 -0800

    Wrote very useful get_all_where function.

--- controllers/district.ctrl.php
+++ controllers/district.ctrl.php
@@ -1,16 +1,20 @@
 <?php
 
-if (!empty($args)) {
+if (!empty($args[0])) {
     $dist_name = str_replace("_", " ", $args[0]);
     $dist_id = $db->query("SELECT id FROM district WHERE name = '$dist_name' LIMIT 1")[0]["id"];
+    show_schools($dist_id, $dist_name);
 }
-if (!empty($dist_id)) {
-    $dist = new \models\EGR_District($dist_id);
-    // need function get_all_models($model, $where); returns array of models of type $model
-    $obj = json_encode($dist, JSON_PRETTY_PRINT);
-    $page["body"] = "<pre>" . $obj . "</pre>";
-    $page["title"] = $dist->name;
-    template("default", $page);
-} else {
-    require_once "controllers/404.ctrl.php";
+
+function show_schools($dist_id, $dist_name) {
+    if (!empty($dist_id)) {
+        $schools = \models\get_all_where("school", "district = '$dist_id'");
+        // need function get_all_models($model, $where); returns array of models of type $model
+        $obj = json_encode($schools, JSON_PRETTY_PRINT);
+        $page["body"] = "<pre>" . $obj . "</pre>";
+        $page["title"] = $dist_name;
+        template("default", $page);
+    } else {
+        require_once "controllers/404.ctrl.php";
+    }
 }
