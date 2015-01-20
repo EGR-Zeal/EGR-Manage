814a87f77a715c1434484d7a6d8b116b94afa8f6
EGR-Zeal <zeal@egrobotics.com>
2015-01-20 13:42:24 -0800

    Router function BOSSMODE

--- controllers/district.ctrl.php
+++ controllers/district.ctrl.php
@@ -3,7 +3,12 @@
 if (!empty($args[0])) {
     $dist_name = str_replace("_", " ", $args[0]);
     $dist_id = $db->query("SELECT id FROM district WHERE name = '$dist_name' LIMIT 1")[0]["id"];
-    show_schools($dist_id, $dist_name);
+    if (empty($args[1])) {
+        show_schools($dist_id, $dist_name);
+    } else {
+        $school_name = str_replace("_", " ", $args[1]);
+        $school_id = $db->query("SELECT id FROM school WHERE district = '$dist_id' AND name = '$school_name' LIMIT 1")[0]["id"];
+    }
 } else {
     header("Location: http://egrobotics.com/dev/app/districts/");
 }
@@ -12,8 +17,8 @@ function show_schools($dist_id, $dist_name) {
     if (!empty($dist_id)) {
         $schools = \models\get_all_where("school", "district = '$dist_id'");
         // need function get_all_models($model, $where); returns array of models of type $model
-        foreach($schools as $school){
-            $page["body"] .= "<a href='#'>" . $school->name . "</a>\n";
+        foreach ($schools as $school) {
+            $page["body"] .= "<a href='" . str_replace(" ", "_", $school->name) . "'>" . $school->name . "</a>\n";
         }
         $page["title"] = $dist_name;
         template("default", $page);
---
+++ test.php
@@ -0,0 +1,32 @@
+<?php
+
+$controllers = [
+    "districts" => "districts",
+    "district" => "district/{distname}/{schoolname}"
+];
+
+function match_route($request, $route) {
+    //get URL argument names with preg_match_all
+    $var_matches = [];
+    $num_vars = preg_match_all("/\{([^\}]+)\}/", $route, $var_matches);
+    $var_names = $var_matches[1];
+    //replace URL arguments with regex ([^\/]+)
+    $route_pattern_quote = preg_quote($route);
+    $route_pattern = "|" . preg_replace("/\\\\{[^\\\]+\\\\}/", "([^\/]+)", $route_pattern_quote) . "|";
+    //attempt a match
+    $matches = [];
+    $num_matches = preg_match_all($route_pattern, $request, $matches);
+    if($num_matches === 0){
+        return FALSE;
+    }
+    array_shift($matches);
+    //assign matches to array with var_names as keys
+    $ret = [];
+    foreach ($matches as $i => $match) {
+        $ret[$var_names[$i]] = $match[0];
+    }
+    return $ret;
+}
+
+$vars = match_route("district/Portland_Public_Schools/Alameda", $controllers["district"]);
+var_dump($vars);
